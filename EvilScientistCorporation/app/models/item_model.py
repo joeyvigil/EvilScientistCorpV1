0383cc49568c1805cc392b8e98d4943329cc38f0
benp3837 <petruzziellbj@alumni.vcu.edu>
2026-01-08 09:47:07 -0500

    ItemModel

--- EvilScientistCorporation/app/models/item_model.py
+++ EvilScientistCorporation/app/models/item_model.py
@@ -1,1 +1,16 @@
-# empty for now
+from typing import Annotated, Optional
+
+from pydantic import BaseModel, Field
+
+# Check user_model for relevant notes
+
+class ItemModel(BaseModel):
+    # Optional - another way to make this value optional
+    # like setting "= None" like we did in UserModel
+    id: Optional[int, Field(gt=0)]
+    name: Annotated[str, Field(min_length=3, max_length=50)]
+    description: Annotated[str, Field(min_length=10, max_length=100)]
+    # ge = greater than or equal to
+    # lt = less than or equal to
+    inventory: Annotated[int, Field(ge=0, le=100)]
+    price: Annotated[float, Field(gt=0)]
